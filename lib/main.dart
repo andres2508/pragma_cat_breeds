09d271b2a3f98dd4d706865d2dc68c56093ff5f6
andres2508 <jaime.aristizabal.2508@gmail.com>
2024-02-22 00:48:12 -0500

    Fix, splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,15 @@ import 'package:cat_breeds/infrastructure/http/http.data_source.dart';
 import 'package:cat_breeds/infrastructure/http/http.interceptor.dart';
 import 'package:cat_breeds/infrastructure/locator/service.locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   setupServiceLocator();
   _registerInterceptors();
   runApp(PragmaMobileApp());
+  FlutterNativeSplash.remove();
 }
 
 void _registerInterceptors() {
